1f9954fceaf580bdb71333e6e72e70fc2a402616
kelvinlee0225 <leekelvin0225@gmail.com>
2023-06-20 10:02:31 -0400

    add mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,15 +27,36 @@ export function NavBar() {
       <div className="sm:hidden flex flex-1 justify-end items-center">
         {toggle ? (
           <RiCloseFill
+            alt="menu opened"
             className="text-white h-8 w-7"
             onClick={() => setToggle((previous) => !previous)}
           />
         ) : (
           <RiMenu3Fill
+            alt="menu"
             className="text-white h-8 w-5"
             onClick={() => setToggle((previous) => !previous)}
           />
         )}
+      </div>
+
+      <div
+        className={`${
+          toggle ? "flex" : "hidden"
+        } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+      >
+        <ul className="list-none flex flex-col justify-end items-center flex-1">
+          {navLinks.map((nav, index) => (
+            <li
+              key={nav.id}
+              className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+                index === navLinks.length - 1 ? "mr-0" : "mb-4"
+              }`}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export function Stats() {
   return <div>Stats</div>;
 }
