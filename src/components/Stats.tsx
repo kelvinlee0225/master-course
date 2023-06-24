610016a1b7ad85a6da9ed05d79e64fe4c5340d62
kelvinlee0225 <leekelvin0225@gmail.com>
2023-06-24 07:00:00 -0400

    update stats component

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,3 +1,40 @@
+import styles from "../style";
+import { StatsConstants } from "../constants";
+import { BsStarFill, BsStarHalf } from "react-icons/bs";
+
 export function Stats() {
-  return <div>Stats</div>;
+  return (
+    <section
+      className={`md:${styles.paddingY} flex justify-around flex-row flex-wrap w-full `}
+    >
+      {StatsConstants.map((stat, index) => {
+        return (
+          <div className={`${styles.flexCenter}`}>
+            <h1
+              key={stat.id}
+              className={`text-white font-semibold font-poppins  ${
+                index === StatsConstants.length - 1 ? "ml-4" : "mr-4"
+              } ss:text-[40px] text-[30px] flex flex-row items-center`}
+            >
+              {stat.value}
+              {stat.id === "stats-2" && (
+                <div className="flex flex-row ml-2">
+                  {Array(+stat.value[0])
+                    .fill(1)
+                    .map(() => (
+                      <BsStarFill />
+                    ))}
+                  <BsStarHalf />
+                </div>
+              )}
+              <span className="font-normal text-gradient ss:text-[30px] text-[22px] ml-4 md:my-0 my-4">
+                {" "}
+                {stat.title.toUpperCase()}
+              </span>
+            </h1>
+          </div>
+        );
+      })}
+    </section>
+  );
 }
---
+++ src/constants/StatsConstants.ts
@@ -0,0 +1,12 @@
+export const StatsConstants = [
+  {
+    id: "stats-1",
+    title: "Students",
+    value: "1,000,000+",
+  },
+  {
+    id: "stats-2",
+    title: "(467,189 ratings)",
+    value: "4.5",
+  },
+];
