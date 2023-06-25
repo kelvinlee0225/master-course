53d6b36ddebdad4a35cbd7dafb1feb4c9f06b1d0
kelvinlee0225 <leekelvin0225@gmail.com>
2023-06-25 17:37:33 -0400

    added more about career constants

---
+++ src/constants/AboutCareerConstants.ts
@@ -0,0 +1,28 @@
+export const AboutCareerConstants = [
+  {
+    id: "about-1",
+    title: "Introduction",
+    value:
+      "In this career, you will grasp the significance of data interpretation in establishing business strategies. To achieve this, you will learn the concepts and general foundations of data processing. You will acquire the ability to analyze data more effectively using Tableau, SQL, and R Programming.",
+  },
+  {
+    id: "about-2",
+    title: "Requirements",
+    value: "No prior knowledge is required.",
+  },
+  {
+    id: "about-3",
+    title: "What you'll learn",
+    value: [
+      "- Prepare, analyze, visualize, and share your insights derived from large volumes of data using Tableau.",
+      "- Gain an understanding of data cleaning and organization techniques for analysis purposes. Additionally, develop proficiency in performing analysis and calculations using SQL and R programming..",
+      "- Acquire the skills to effectively visualize and present data insights using dashboards, presentations, and popular visualization platforms.",
+    ],
+  },
+  {
+    id: "about-4",
+    title: "Who certifies us?",
+    value:
+      "Our courses are certified by leading companies in each industry such as Uber, Meta, Google, IMDB, Airbnb, among others.",
+  },
+];
